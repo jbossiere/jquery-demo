cae04ba53ab50c3933607acafa1ed290f75b1c93
iGuest <julianbossiere@gmail.com>
2015-10-26 14:23:17 -0700

    Made changes

--- js/main.js
+++ js/main.js
@@ -2,3 +2,35 @@
 
 /* Use clever selections and event handlers to assign the described interactivity */
 
+$('button').click(function(){
+    var button = $(this).attr('id');
+    var selector = $('#selector').val();
+    var newContent = $('#newContent').val();
+
+    if (button == "change-content") {
+        $(selector).html(newContent);
+
+    } else if (button == "add-at-end") {
+        $(selector).append('<div>' + newContent + '</div>');
+
+    } else if (button == "add-at-start") {
+        $(selector).prepend('<div>' + newContent + "</div>");
+
+    } else if (button == "insert-before") {
+        $("<div>" + newContent + "</div>").insertBefore(selector)
+
+    } else if (button == "move-after") {
+
+
+    } else if (button == "surround-class") {
+
+
+    } else if (button == "hide-text") {
+
+
+    } else if (button == "remove-word") {
+        $(selector).replace(newContent, "")
+
+    } 
+})
+
